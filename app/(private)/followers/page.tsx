118b810492017b1d56dad2da57178ac3a69fd935
nisha4L <kyawzint917@gmail.com>
2024-03-14 22:26:20 +0630

    follower page.endpoint , following page.endpoint

--- app/(private)/followers/page.tsx
+++ app/(private)/followers/page.tsx
@@ -1,7 +1,11 @@
-export default async function Followers() {
+"use client";
+import FollowersContainer from "./followers-container";
+
+export default function Followers() {
   return (
     <main>
       <h2>Followers</h2>
+      <FollowersContainer />
     </main>
   );
 }
--- app/(private)/following/page.tsx
+++ app/(private)/following/page.tsx
@@ -1,7 +1,11 @@
-export default async function Following() {
+"use client";
+import FollowingContainer from "./following-container";
+
+export default function Following() {
   return (
     <main>
       <h2>Following</h2>
+      <FollowingContainer />
     </main>
   );
 }
