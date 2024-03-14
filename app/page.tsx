77608d365b23808615ab54ac8010f896e681cca8
nisha4L <kyawzint917@gmail.com>
2024-03-14 12:55:21 +0630

    initial migration

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <div className="flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-800">
+      <div className="flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-800 rounded-lg">
         <div className="text-center my-4">
           <h1 className="font-semibold text-xl">SpaceX</h1>
         </div>
