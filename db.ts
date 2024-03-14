f8b7e05397b7cd6cab1f7d0490a89acdaeb573cd
nisha4L <kyawzint917@gmail.com>
2024-03-14 15:13:11 +0630

    install jose . login endpoint

--- db.ts
+++ db.ts
@@ -5,18 +5,28 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 export async function getClient(): Promise<Client> {
-  if (process.env.POSTGRES_URL) {
+
+    if (process.env.POSTGRES_URL_NO_SSL) {
+        const client = new Client({
+            connectionString : process.env.POSTGRES_URL_NO_SSL + "?sslmode=require",
+        })
+        return client;
+    }
+
     const client = new Client({
-      connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_NAME,
+        password: process.env.POSTGRES_PASSWORD,
+        port: parseInt(process.env.POSTGRES_PORT!)
     });
     return client;
-  }
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_NAME,
-    password: process.env.POSTGRES_PASSWORD,
-    port: parseInt(process.env.POSTGRES_PORT!),
-  });
-  return client;
+};
+
+export async function sql(sql: string, values?: Array<any>): Promise<QueryResult<any>> {
+    const client = await getClient();
+    await client.connect();
+    const res = await client.query(sql, values);
+    await client.end();
+    return res;
 }
