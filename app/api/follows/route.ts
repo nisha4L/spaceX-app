f0c348dec121a10156eb328ce6601a6487c633de
nisha4L <kyawzint917@gmail.com>
2024-03-15 13:27:56 +0630

    delete follow endpoint

--- app/api/follows/route.ts
+++ app/api/follows/route.ts
@@ -3,13 +3,34 @@ import { sql } from "@/db";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const jwtPayload = await getJWTPayload();
-    const { searchParams } = new URL(request.url);
-    const userId = searchParams.get("user_id");
+  const jwtPayload = await getJWTPayload();
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("user_id");
 
-    const res = await sql(
-        "select * from follows where user_id = $1 and follower_id = $2",
-        [userId, jwtPayload.sub]
-    );
-    return NextResponse.json({ data: res.rows });
+  const res = await sql(
+    "select * from follows where user_id = $1 and follower_id = $2",
+    [userId, jwtPayload.sub]
+  );
+  return NextResponse.json({ data: res.rows });
+}
+
+export async function POST(request: Request) {
+  const jwtPayload = await getJWTPayload();
+  const json = await request.json();
+
+  const res = await sql(
+    "select * from follows where user_id = $1 and follower_id = $2",
+    [json.user_id, jwtPayload.sub]
+  );
+//@ts-ignore
+  if (res.rowCount > 0) {
+    return NextResponse.json({ error: "already following" }, { status: 409 });
+  }
+
+  await sql("insert into follows (user_id, follower_id) values ($1, $2)", [
+    json.user_id,
+    jwtPayload.sub,
+  ]);
+
+  return NextResponse.json({ msg: "follow success" });
 }
