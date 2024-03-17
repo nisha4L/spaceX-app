0bac52a838c0fd6fdc063912f8dca4b46af33714
nisha4L <kyawzint917@gmail.com>
2024-03-17 10:34:20 +0630

    cron job

--- middleware.ts
+++ middleware.ts
@@ -2,33 +2,43 @@ import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
+  const pathname = request.nextUrl.pathname;
 
-    const authenticatedAPIRoutes = [
-        pathname.startsWith("/api/users"),
-        pathname.startsWith("/api/posts"),
-        pathname.startsWith("/api/follows"),
-        pathname.startsWith("/api/admin"),
-        pathname.startsWith("/api/search")
-    ];
+  const authenticatedAPIRoutes = [
+    pathname.startsWith("/api/users"),
+    pathname.startsWith("/api/posts"),
+    pathname.startsWith("/api/follows"),
+    pathname.startsWith("/api/admin"),
+    pathname.startsWith("/api/search"),
+  ];
 
-    if (authenticatedAPIRoutes.includes(true)) {
-        const cookie = request.cookies.get("jwt-token")
+  const authenticatedCronRoutes = [pathname.startsWith("/api/cron")];
 
-        if (!cookie || !cookie?.value) {
-            return NextResponse.json({ error: "Unauthenticated" }, { status: 401 });
-        }
+  if (authenticatedAPIRoutes.includes(true)) {
+    const cookie = request.cookies.get("jwt-token");
 
-        try {
-            const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
-            await jwtVerify(cookie.value, secret);
-        } catch (error) {
-            console.error(error);
-            return NextResponse.json({ error: "Unauthenticated" }, { status: 401 });
-        }
+    if (!cookie || !cookie?.value) {
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
     }
+
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+      await jwtVerify(cookie.value, secret);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
+    }
+  }
+
+  if (authenticatedCronRoutes.includes(true)) {
+    const key = request.nextUrl.searchParams.get("cron_api_key");
+    const isAuthenticated = key === process.env.CRON_API_KEY;
+    if (!isAuthenticated) {
+      return NextResponse.json({ error: "unauthenticated" }, { status: 401 });
+    }
+  }
 }
 
 export const config = {
-    matcher: "/:path*",
-}
+  matcher: "/:path*",
+};
